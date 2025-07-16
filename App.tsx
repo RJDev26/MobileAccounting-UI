f58b7dcb10a52bd08b09ca0632354f8acc5929c6
puneeshk <puneeshkapoor1990@gmail.com>
2025-07-16 18:22:58 +0530

    Added Bottombar

--- App.tsx
+++ App.tsx
@@ -1,39 +1,52 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { StyleSheet, Text, View } from 'react-native'
 import MobileAccounting from './component/mobileAccounting/mobileAccounting'
 import AccountMaster from './component/accountMaster/accountMaster'
 import AddAccount from './component/accountMaster/addAccount'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 const Stack = createNativeStackNavigator();
 
+const Tab = createBottomTabNavigator();
+
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='MobileAccounting'>
-        <Stack.Screen
-          name="MobileAccounting"
-          component={MobileAccounting}
-          options={{
-            headerShown: false
-          }}
-        />
-        <Stack.Screen
-          name="AccountMaster"
-          component={AccountMaster}
-          options={{
-            headerShown: false
-          }}
-        />
-        <Stack.Screen
-          name="AddAccount"
-          component={AddAccount}
-          options={{
-            headerShown: false
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      {/* <NavigationContainer>
+        <Stack.Navigator initialRouteName='MobileAccounting'>
+          <Stack.Screen
+            name="MobileAccounting"
+            component={MobileAccounting}
+            options={{
+              headerShown: false
+            }}
+          />
+          <Stack.Screen
+            name="AccountMaster"
+            component={AccountMaster}
+            options={{
+              headerShown: false
+            }}
+          />
+          <Stack.Screen
+            name="AddAccount"
+            component={AddAccount}
+            options={{
+              headerShown: false
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer> */}
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="MobileAccounting" component={MobileAccounting} options={{ headerShown: false, tabBarStyle: { display: 'none' } }} />
+          <Tab.Screen name="AccountMaster" component={AccountMaster} options={{ headerShown: false }} />
+          {/* <Tab.Screen name="AddAccount" component={AddAccount} options={{ headerShown: false }} /> */}
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
